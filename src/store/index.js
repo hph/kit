4f3f203ebe83c31f87de3a127f01726b751ee711
Haukur Páll Hallvarðsson <hph@hph.is>
2016-05-10 00:51:04 +0000

    Add localStorage support

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
+import persistState from 'redux-localstorage';
 
 import rootReducer from 'reducers';
 
 
 export default function configureStore (initialState) {
   const extension = window.devToolsExtension && window.devToolsExtension();
-  const store = createStore(rootReducer, initialState, extension);
+
+  const createPersistentStore = compose(persistState())(createStore);
+  const store = createPersistentStore(rootReducer, initialState, extension);
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
